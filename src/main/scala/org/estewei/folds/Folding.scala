f44a9108da7fe035eba986182afec9505308b655
Emrys Ingersoll <ingersoll@gmail.com>
2014-08-17 11:36:32 -0700

    Move some L1 combinator definitions into L1 itself

--- src/main/scala/org/estewei/folds/Folding.scala
+++ src/main/scala/org/estewei/folds/Folding.scala
@@ -14,13 +14,13 @@ trait Folding[P[_, _]] extends Scan[P] {
 
   def filtering[A, B](p: A => Boolean): P[A, B] => P[A, B]
 
-  def prefix[T[_], A, B](ta: T[A])(implicit T: Foldable[T]): P[A, B] => P[A, B] =
+  def prefix[T[_]: Foldable, A, B](ta: T[A]): P[A, B] => P[A, B] =
     prefixOf(Fold[T, A], ta)
 
-  def postfix[T[_], A, B](p: P[A, B])(implicit T: Foldable[T]): T[A] => P[A, B] =
+  def postfix[T[_]: Foldable, A, B](p: P[A, B]): T[A] => P[A, B] =
     postfixOf(Fold[T, A], p)
 
-  def run[T[_], A, B](ta: T[A])(implicit T: Foldable[T]): P[A, B] => B =
+  def run[T[_]: Foldable, A, B](ta: T[A]): P[A, B] => B =
     runOf(Fold[T, A], ta)
 
 }
--- src/main/scala/org/estewei/folds/L1.scala
+++ src/main/scala/org/estewei/folds/L1.scala
@@ -9,6 +9,28 @@ sealed abstract class L1[A, B] {
   val k: C => B
   val h: C => A => C
   val z: A => C
+
+  def run1(a: A): B =
+    k(z(a))
+
+  def map[D](f: B => D): L1[A, D] =
+    L1[A, D, C](f compose k, h, z)
+
+  def flatMap[D](f: B => L1[A, D]): L1[A, D] =
+    L1[A, D, (OneAnd[DList, A], B)](
+      t => f(t._2) walk t._1,
+      t => a => (OneAnd(t._1.head, t._1.tail :+ a), t._2),
+      a => (OneAnd(a, DList[A]()), k(z(a))))
+
+  def compose[D](g: L1[D, A]): L1[D, B] =
+    L1[D, B, (C, g.C)](
+      cc => k(cc._1),
+      cc => a => { val y = g.h(cc._2)(a); (h(cc._1)(g.k(y)), y) },
+      a => { val y = g.z(a); (z(g.k(y)), y) })
+
+  private def walk(xs: OneAnd[DList, A]): B =
+    k(xs.tail.foldr(z(xs.head))((a, c) => h(c)(a)))
+
 }
 
 object L1 {
@@ -23,7 +45,7 @@ object L1 {
     new Scan[L1] with Arrow[L1] {
 
       def run1[A, B](a: A, p: L1[A, B]): B =
-        p.k(p.z(a))
+        p run1 a
 
       def prefix1[A, B](a: A, p: L1[A, B]): L1[A, B] =
         L1(p.k, p.h, p.h(p.z(a)))
@@ -61,31 +83,19 @@ object L1 {
         arr(identity)
 
       def compose[A, B, C](f: L1[B, C], g: L1[A, B]): L1[A, C] =
-        L1[A, C, (f.C, g.C)](
-          cc => f.k(cc._1),
-          cc => a => { val y = g.h(cc._2)(a); (f.h(cc._1)(g.k(y)), y) },
-          a => { val y = g.z(a); (f.z(g.k(y)), y) }
-        )
+        f compose g
     }
 
   implicit def l1Monad[A]: Monad[({type λ[α] = L1[A, α]})#λ] =
     new Monad[({type λ[α] = L1[A, α]})#λ] {
       override def map[B, C](fb: L1[A, B])(f: B => C) =
-        L1[A, C, fb.C](f compose fb.k, fb.h, fb.z)
+        fb map f
 
       def point[B](b: => B): L1[A, B] =
         L1[A, B, Unit](_ => b, _ => _ => (), _ => ())
 
       def bind[B, C](fb: L1[A, B])(f: B => L1[A, C]): L1[A, C] =
-        L1[A, C, (OneAnd[DList, A], B)](
-          t => walk(t._1)(f(t._2)),
-          t => a => (OneAnd(t._1.head, t._1.tail :+ a), t._2),
-          a => (OneAnd(a, DList[A]()), fb.k(fb.z(a))))
+        fb flatMap f
     }
-
-  ////
-
-  private def walk[A, B](xs: OneAnd[DList, A])(l1: L1[A, B]): B =
-    l1.k(xs.tail.foldr(l1.z(xs.head))((a, c) => l1.h(c)(a)))
 
 }
